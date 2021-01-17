0890589ad4c21e34841022318e7014c13cbeee39
cmyUE <kaitomikkumiku+github@gmail.com>
2021-01-17 17:11:13 +0900

    [add] プロジェクトを作りました。

---
+++ app/src/main/java/com/nitok_ict/socialgen/socialgen_app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.nitok_ict.socialgen.socialgen_app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
