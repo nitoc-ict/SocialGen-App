8efbb1ece2ebdabc6e98aaf4e65f9a081f8cdf72
cmyUE <kaitomikkumiku+github@gmail.com>
2021-01-18 18:05:35 +0900

    [W.I.P] Ranking機能実装中

--- app/src/main/java/com/nitok_ict/socialgen/socialgen_app/model/ServerCommunicationModel.kt
+++ app/src/main/java/com/nitok_ict/socialgen/socialgen_app/model/ServerCommunicationModel.kt
@@ -2,7 +2,7 @@ package com.nitok_ict.socialgen.socialgen_app.model
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import sun.security.pkcs11.wrapper.CK_C_INITIALIZE_ARGS
+//import sun.security.pkcs11.wrapper.CK_C_INITIALIZE_ARGS
 
 data class UserRank(val userName:String,
                     val rank:Int,
@@ -12,7 +12,11 @@ class ServerCommunicationModel()
 {
     fun getRanking():List<UserRank>{
 
-        return listOf(UserRank("hoge", 3, 1245))
+        return listOf(
+            UserRank("hoge", 1, 3939),
+            UserRank("fuga", 2, 3737),
+            UserRank("poyo", 3, 710)
+            )
     }
 
 }
--- app/src/main/java/com/nitok_ict/socialgen/socialgen_app/ui/ranking/RankingViewModel.kt
+++ app/src/main/java/com/nitok_ict/socialgen/socialgen_app/ui/ranking/RankingViewModel.kt
@@ -3,8 +3,15 @@ package com.nitok_ict.socialgen.socialgen_app.ui.ranking
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nitok_ict.socialgen.socialgen_app.model.ServerCommunicationModel
+import com.nitok_ict.socialgen.socialgen_app.model.UserRank
 
 class RankingViewModel : ViewModel() {
+
+    private val rankingModel = ServerCommunicationModel()
+
+    val _rankingDataList = rankingModel.getRanking()
+    val rankingLiveData: LiveData<UserRank> = _rankingDataList
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
